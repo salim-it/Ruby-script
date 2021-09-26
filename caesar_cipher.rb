4e814da3e76173d09b4fef6d1010695f91c7f6f5
salim-it <finisse@hotmail.com>
2021-09-26 17:52:35 -0300

    first commit

---
+++ caesar_cipher.rb
@@ -0,0 +1,40 @@
+def caesar_cipher(string,shift_factor)
+
+    # Split string into character array, get ascii value for each character 
+    # 65 - 90 for A-B
+    # 97 - 122 for a-b
+    asciis = string.split("").map { |e| e.ord }
+
+    shifted = asciis.map do |ascii|
+        # check if character is aplhabetic, yes - do shift , no - no shift
+        if ascii.between?(65,90)
+            # logic for wrap around
+            if shift_factor.positive?
+                ascii + shift_factor > 90 ? (ascii + shift_factor) - 26 : ascii + shift_factor
+            else
+                ascii + shift_factor < 65 ? (ascii + shift_factor) + 26 : ascii + shift_factor
+            end
+        elsif ascii.between?(97,122)
+            if shift_factor.positive?
+                ascii + shift_factor > 122 ? (ascii + shift_factor) - 26 : ascii + shift_factor
+            else
+                ascii + shift_factor < 97 ? (ascii + shift_factor) + 26 : ascii + shift_factor
+            end
+        else
+            ascii
+        end
+    end
+
+    shifted_string = shifted.map! { |a| a.chr }.join
+    
+	return shifted_string
+
+end
+
+print "Enter the string to encode : "
+string = gets.chomp
+
+print "Enter the shift factor for the string : "
+shift_factor = gets.chomp.to_i
+
+puts caesar_cipher(string,shift_factor)
